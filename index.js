521d8493eb6b9effdf90cb8fdae4ed1f89a78630
Augusto Barrios <augustobg02@gmail.com>
2022-10-12 19:56:25 -0300

    Primer Commit

--- index.js
+++ index.js
@@ -17,23 +17,26 @@ class Usuario{
 
     addMascota(animal){
         /*Nombre de mascotas agregados*/
-        this.mascotas.push(`${animal}`); /* constructorUsuarios.addMascota("animal") */
+        this.mascotas.push(`${animal}`);
+        constructorUsuarios.countMascotas(); /* constructorUsuarios.addMascota("animal") */
         
     };
 
     countMascotas(){
         /*Cantidad de mascotas*/
         for(let i = 0; i<this.mascotas.length; i++){
-            console.log(i + 1)
+            console.log("Tienes " + i + 1 + " mascota/s")
         }
     };
     
     addBook(nombre, autor){
-        this.libros.push({Nombre: nombre, Autor: autor}) /* constructorUsuarios.addBook("libro", "autor") */
+        this.libros.push({Nombre: nombre, Autor: autor})
+        constructorUsuarios.getBooksName(); /* constructorUsuarios.addBook("libro", "autor") */
     }
 
     getBooksName(){
-
+       let getBook = this.libros.map(libro => libro.Nombre);
+       console.log(getBook)
     }
     
 };
@@ -42,6 +45,6 @@ const constructorUsuarios = new Usuario("Augusto", "Barrios Gil",);
 
 console.log(constructorUsuarios);
 /* console.log(constructorUsuarios.getFullName()); */
-constructorUsuarios.addMascota()
-console.log(constructorUsuarios.countMascotas())
-constructorUsuarios.addBook("El cadaver de la novia", "Tim Burton")
+constructorUsuarios.addMascota("pixel");
+constructorUsuarios.addBook("El cadaver de la novia", "Tim Burton");
+
