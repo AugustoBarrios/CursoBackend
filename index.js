c579b3031967fa749c30ad4d3183ea588a8842fe
Augusto Barrios <augustobg02@gmail.com>
2022-10-19 23:02:11 -0300

    ManejoDeArchivos

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /* const animales = document.getElementById("mascota");  
 const cargar = document.getElementById("cargar"); */
 
+/* const inputMascotas = document.getElementById("mascota").innerText;
+const inputLibro = document.getElementById("libro").innerText;
+const inputAutor = document.getElementById("autor").innerText;
+
 class Usuario{
     
     constructor(nombre, apellido,){
@@ -16,14 +20,14 @@ class Usuario{
     };
 
     addMascota(animal){
-        /*Nombre de mascotas agregados*/
-        this.mascotas.push(`${animal}`); /* constructorUsuarios.addMascota("animal") */
+        Nombre de mascotas agregados
+        this.mascotas.push(`${animal}`); constructorUsuarios.addMascota("animal")
         constructorUsuarios.countMascotas(); 
         
     };
 
     countMascotas(){
-        /*Cantidad de mascotas*/
+        Cantidad de mascotas
         for(let i = 0; i<this.mascotas.length; i++){
             console.log("Tienes " + (i + 1)  + " mascota/s")
         }
@@ -31,7 +35,7 @@ class Usuario{
     
     addBook(nombre, autor){
         this.libros.push({Nombre: nombre, Autor: autor})
-        constructorUsuarios.getBooksName(); /* constructorUsuarios.addBook("libro", "autor") */
+        constructorUsuarios.getBooksName(); constructorUsuarios.addBook("libro", "autor")
     }
 
     getBooksName(){
@@ -44,7 +48,32 @@ class Usuario{
 const constructorUsuarios = new Usuario("Augusto", "Barrios Gil",);
 
 console.log(constructorUsuarios);
-/* console.log(constructorUsuarios.getFullName()); */
+console.log(constructorUsuarios.getFullName());
 constructorUsuarios.addMascota("pixel");
 constructorUsuarios.addBook("El cadaver de la novia", "Tim Burton");
 
+ */
+
+class Contenedor{
+    constructor(route){
+        this.route = route
+    }
+    async getAll(){
+        try{
+            const content = await this.getAll()
+            return content
+        } catch(error){
+            console.log(error)
+            return []
+        }
+    }
+    async deletById(id){
+        try{
+            const content = await this.getAll()
+                const elementoFiltrado = content.filter(e => e.id !==id)
+                await fs.writeFile(`./${this.route}`), JSON.stringify(elementoFiltrado, null)
+            }catch(error){
+                console.log(error)
+            }
+    }
+}
