71030a74c39d2049c685c3e1ad8fe32be8fb74c6
Augusto Barrios <augustobg02@gmail.com>
2022-11-08 20:15:13 -0300

    clase 8

--- Clase8/index.js
+++ Clase8/index.js
@@ -1,65 +1,67 @@
 const fs = require('fs')
 const express = require('express')
-const { Console } = require('console')
+/* const { Console } = require('console') */
+const { Router } = express
 
-class Contenedor{
-    constructor(ruta){
+
+class Contenedor {
+    constructor(ruta) {
         this.ruta = ruta
     }
 
-    async save(object){
-        try{
-            const content = JSON.parse( await fs.promises.readFile(`./${this.ruta}`, 'utf-8', ))
+    async save(object) {
+        try {
+            const content = JSON.parse(await fs.promises.readFile(`./${this.ruta}`, 'utf-8',))
             const count = content.length
-            const modificId = content[count - 1].id + 1 
+            const modificId = content[count - 1].id + 1
             object.id = modificId
             const pusheo = content.push(object)
             const add = await fs.promises.writeFile(`./${this.ruta}`, JSON.stringify(content, null, 2))
             await fs.promises.writeFile(`./productos.txt`, JSON.stringify(content, null, 2))
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
     }
 
-     getById(Id){
-        try{
-            const content = JSON.parse(  fs.readFileSync(`./${this.ruta}`, 'utf-8', ))
-            const buscador = content.filter(buscador => buscador.id === JSON.parse(Id))
-            return JSON.stringify(buscador)
-        }catch(error){
+    getById(Id) {
+        try {
+            const content = JSON.parse(fs.readFileSync(`./${this.ruta}`, 'utf-8',))
+            const buscador = JSON.stringify(content.filter(buscador => buscador.id === JSON.parse(Id)))
+            return buscador
+        } catch (error) {
             console.log(error, "no funciono")
         }
     }
 
-    getAll(){
-        try{
-        const content = JSON.parse(  fs.readFileSync(`./${this.ruta}`, 'utf-8', ))
-        const contenteTxt =  fs.readFileSync(`./productos.txt`, 'utf-8', )
-        console.log(contenteTxt)
-        return contenteTxt
-    }catch(error){
-        console.log(error, "no funciono")
-    }}
-
-     deleteById(Id){
-        try{
-            const content = JSON.parse(  fs.promises.readFile(`./${this.ruta}`, 'utf-8', ))
-            const buscador = content.filter(buscador => buscador.id !== JSON.parse(Id))
-            console.log("Tu id es:", Id)
-             fs.promises.writeFile(`./${this.ruta}`, JSON.stringify(buscador, null, 2))
-             fs.promises.writeFile(`./productos.txt`, JSON.stringify(content, null, 2))
-            console.log(`Producto eliminado numero ${Id}`)
-        }catch(error){
+    getAll() {
+        try {
+            const content = JSON.parse(fs.readFileSync(`./${this.ruta}`, 'utf-8',))
+            const contenteTxt = fs.readFileSync(`./productos.txt`, 'utf-8',)
+            console.log(contenteTxt)
+            return contenteTxt
+        } catch (error) {
             console.log(error, "no funciono")
         }
     }
 
-    async deleteAll(){
-        try{
+    deleteById(Id) {
+        try {
+            const content = JSON.parse(fs.readFileSync(`./${this.ruta}`, 'utf-8'))
+            const buscadorParse = content.filter(buscador => buscador.id == JSON.parse(Id))
+            fs.writeFileSync(`./${this.ruta}`, JSON.stringify(buscadorParse, null, 2))
+            const buscadorStringify = content.filter(buscador => buscador.id == Id)
+            fs.writeFileSync(`./productos.txt`, JSON.stringify(buscadorStringify, null, 2))
+        } catch (error) {
+            console.log(error, "no funciono")
+        }
+    }
+
+    async deleteAll() {
+        try {
             await fs.promises.writeFile(`./${this.ruta}`, JSON.stringify([], null, 2))
-            await fs.promises.writeFile(`./productos.txt`, JSON.stringify('' , null, 2))
+            await fs.promises.writeFile(`./productos.txt`, JSON.stringify('', null, 2))
             console.log(`Productos eliminados`)
-        }catch(error){
+        } catch (error) {
             console.log(error, "no funciono")
         }
     }
@@ -67,35 +69,42 @@ class Contenedor{
 }
 
 const ruta = new Contenedor(`array.json`)
-let objInfo = {id:0, titulo:"", descripcion: ""}
-
-/* ruta.getAll();  
-ruta.getById();
-ruta.deleteAll();
-ruta.deleteById();
-ruta.save(objInfo)  */
-
-// NO EJECUTAR ruta.save(objInfo) CON NODEMON ACTIVADO
+const contentOfArrayJson =  fs.readFileSync(`./array.json`, 'utf-8',)
+/* let objInfo = { id: 0, titulo: "", descripcion: "" } */
 
 
 const app = express();
-const port = 8080; 
+const router = Router();
 
-const server = app.listen(port, ()=>{
+const port = 8080;
+
+const server = app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 })
 
-app.get('/api/productos', (req, res)=>{
+router.get("/productos", (req, res) => {
     res.send(`Lo has logrado ${ruta.getAll()}`)
 })
 
-app.get(`/api/productos/:id`, (req, res)=>{
+router.get(`/productos/:id`, (req, res) => {
     let IdRequest = req.params.id;
     res.send(`Lo has logrado ${ruta.getById(IdRequest)}`)
 })
 
-/* app.delete(`/api/productos/:id`, (req, res)=>{
+router.delete(`/eliminarproductos/:id`, (req, res) => {
     let IdRequest = req.params.id;
-    res.send(`Has eliminado este el producto ${ruta.deleteById(IdRequest)}`)
-}) */
+    const deleted = contentOfArrayJson.find(object => object.id === IdRequest) //HOLA PORFE ESTOY TRABADO EN ESTE DELETE NO SE PORQUE NO ME FUNCIONA, PROBE USAR EL "RUTA.DELETBYID(2), Y TAMPOCO ME FUNCIONA"
+    console.log(deleted)
+    res.json({
+       
+    })
+})
+
+
+app.use("/api", router)
+
+
+
+
+
 
