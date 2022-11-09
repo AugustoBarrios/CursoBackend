69d2386520614d0c7dd1e31c8e2c959d6afcb12d
Augusto Barrios <augustobg02@gmail.com>
2022-11-09 01:41:00 -0300

    Router & Multer

--- Clase8/index.js
+++ Clase8/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const express = require('express')
+const { stringify } = require('querystring')
 /* const { Console } = require('console') */
 const { Router } = express
 
@@ -9,15 +10,16 @@ class Contenedor {
         this.ruta = ruta
     }
 
-    async save(object) {
+     save(object) {
         try {
-            const content = JSON.parse(await fs.promises.readFile(`./${this.ruta}`, 'utf-8',))
+            const content = JSON.parse( fs.readFileSync(`./${this.ruta}`, 'utf-8',))
             const count = content.length
             const modificId = content[count - 1].id + 1
             object.id = modificId
             const pusheo = content.push(object)
-            const add = await fs.promises.writeFile(`./${this.ruta}`, JSON.stringify(content, null, 2))
-            await fs.promises.writeFile(`./productos.txt`, JSON.stringify(content, null, 2))
+            console.log(pusheo)
+            const add =  fs.writeFileSync(`./${this.ruta}`, JSON.stringify(content, null, 2))
+             fs.writeFileSync(`./productos.txt`, JSON.stringify(content, null, 2))
         } catch (error) {
             console.log(error)
         }
@@ -37,7 +39,6 @@ class Contenedor {
         try {
             const content = JSON.parse(fs.readFileSync(`./${this.ruta}`, 'utf-8',))
             const contenteTxt = fs.readFileSync(`./productos.txt`, 'utf-8',)
-            console.log(contenteTxt)
             return contenteTxt
         } catch (error) {
             console.log(error, "no funciono")
@@ -47,10 +48,14 @@ class Contenedor {
     deleteById(Id) {
         try {
             const content = JSON.parse(fs.readFileSync(`./${this.ruta}`, 'utf-8'))
-            const buscadorParse = content.filter(buscador => buscador.id == JSON.parse(Id))
-            fs.writeFileSync(`./${this.ruta}`, JSON.stringify(buscadorParse, null, 2))
-            const buscadorStringify = content.filter(buscador => buscador.id == Id)
-            fs.writeFileSync(`./productos.txt`, JSON.stringify(buscadorStringify, null, 2))
+            const buscadorParse  = content.findIndex(buscador => buscador.id == JSON.parse(Id))
+            const borrador = content.splice(buscadorParse, 1)
+            console.log(buscadorParse)
+            console.log(content)
+            if(buscadorParse >= 0){
+                 fs.writeFileSync(`./${this.ruta}`, JSON.stringify(content, null, 2)) 
+             fs.writeFileSync(`./productos.txt`, JSON.stringify(content, null, 2)) 
+            }else{}
         } catch (error) {
             console.log(error, "no funciono")
         }
@@ -69,42 +74,43 @@ class Contenedor {
 }
 
 const ruta = new Contenedor(`array.json`)
-const contentOfArrayJson =  fs.readFileSync(`./array.json`, 'utf-8',)
 /* let objInfo = { id: 0, titulo: "", descripcion: "" } */
 
-
 const app = express();
+app.use(express.static(__dirname + '/public')); //Aqui
+app.use(express.urlencoded({extended: true}))
 const router = Router();
-
+router.use(express.json());
 const port = 8080;
 
 const server = app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 })
 
-router.get("/productos", (req, res) => {
+router.get("/users", (req, res) => {
     res.send(`Lo has logrado ${ruta.getAll()}`)
 })
 
-router.get(`/productos/:id`, (req, res) => {
+router.get(`/user/:id`, (req, res) => {
     let IdRequest = req.params.id;
     res.send(`Lo has logrado ${ruta.getById(IdRequest)}`)
 })
 
-router.delete(`/eliminarproductos/:id`, (req, res) => {
+router.post(`/post`, (req, res)=>{
+    const { body } = req;
+    console.log(body)
+    ruta.save(body)
+    res.json({Mensaje: "producto agregado"})
+
+})
+
+router.delete(`/delete/:id`, (req, res) => {
     let IdRequest = req.params.id;
-    const deleted = contentOfArrayJson.find(object => object.id === IdRequest) //HOLA PORFE ESTOY TRABADO EN ESTE DELETE NO SE PORQUE NO ME FUNCIONA, PROBE USAR EL "RUTA.DELETBYID(2), Y TAMPOCO ME FUNCIONA"
-    console.log(deleted)
-    res.json({
-       
-    })
+    const product = ruta.deleteById(IdRequest);
+    res.json({Mensaje:`Eliminaste con exito el usuario/producto con el id: ${IdRequest} `})
 })
 
 
 app.use("/api", router)
 
-
-
-
-
-
+// POSTMAN (https://web.postman.co/workspace/My-Workspace~d6b47eb3-02dd-4cc6-8244-b85bd27af5f9/request/create?requestId=5a26a350-5e40-4509-a2ab-c5e2d5887d5f)
